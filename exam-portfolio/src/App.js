858790bb93d51d87a5b079d26f537b951196b016
NEL ANTHONY L. SALAR <NLSALAR@Complab2.vsuisabel>
2025-11-26 09:46:11 +0800

    Add Navbar, Hero, Footer components and update App.js

---
+++ exam-portfolio/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './Navbar';
+import Hero from './Hero';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ exam-portfolio/src/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Hero() {
+  return (
+    <section>
+      <h2>Welcome to My Portfolio</h2>
+      <p>This is the hero section of the page.</p>
+    </section>
+  );
+}
+
+export default Hero;
